c6d1f3c7c1e2fef4794e96fe13d2d0ce11bb70ff
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2023-12-06 00:46:29 -0300

    Finalização do Projeto

--- src/main/java/org/example/RespostaJSON.java
+++ src/main/java/org/example/RespostaJSON.java
@@ -1,2 +1,13 @@
-package org.example;public class RespostaJSON {
+package org.example;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class RespostaJSON {
+    List<Universidade> universidades;
+
 }
--- src/main/java/org/example/Universidade.java
+++ src/main/java/org/example/Universidade.java
@@ -4,20 +4,26 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.json.JSONArray;
+
 import java.util.List;
 
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Entrada {
+public class Universidade {
     @JsonProperty("name")
     private String name;
     @JsonProperty("web_pages")
-    private List<String> url;
+    private JSONArray url;
 
+    public Universidade(String name, JSONArray url){
+        this.name = name;
+        this.url = url;
+    }
     @Override
     public String toString(){
-        return "Entrada{" +
+        return "Universidade{" +
                 "Name = " + getName() + " / " +
                 "URLs = " + getUrl() + "}";
     }
